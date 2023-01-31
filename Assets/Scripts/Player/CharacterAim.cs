d9c1a784b3303201015f4616a312ab5379ffd9b6
CleryJa <daxxas158@gmail.com>
2023-01-31 22:17:35 +0100

    started working on aim animation

---
+++ Assets/Scripts/Player/CharacterAim.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAim : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Camera camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        // TODO : Watch this : https://www.youtube.com/watch?v=Q56quIB2sOg&ab_channel=TheKiwiCoder
+    }
+}
