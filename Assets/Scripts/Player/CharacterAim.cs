a5493c4603e864aec3ae69fe4bbfbd5b6ce3c0dc
Jacques-Portable <daxxas158@gmail.com>
2023-02-03 12:45:45 +0100

    kinda working aim target ik

--- Assets/Scripts/Player/CharacterAim.cs
+++ Assets/Scripts/Player/CharacterAim.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 
 public class CharacterAim : MonoBehaviour
 {
-    [SerializeField] private Transform handIk;
-    [SerializeField] private Animator animator;
+    [SerializeField] private Transform aimTargetIK;
+    [SerializeField] private Vector3 targetOffset;
+    [SerializeField] private Vector3 rayStartPositionOffset;
     [SerializeField] private Camera camera;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float aimTargetMaxDistance = 10f;
+    [SerializeField] private float aimSharpness = 1f;
+    [SerializeField] private LayerMask aimable;
 
+    private Vector3 aimTargetPosition;
+    
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + rayStartPositionOffset  + targetOffset, camera.transform.forward, out hit, aimTargetMaxDistance, aimable))
+        {
+            aimTargetPosition = hit.point + targetOffset;
+        }
+        else
+        {
+            Vector3 cameraForward = camera.transform.forward * aimTargetMaxDistance;
+            aimTargetPosition = transform.position + cameraForward + targetOffset;
+        }
 
-    private void OnAnimatorIK(int layerIndex)
-    {
-        // TODO : Watch this : https://www.youtube.com/watch?v=Q56quIB2sOg&ab_channel=TheKiwiCoder
+        aimTargetIK.position = Vector3.Slerp(aimTargetIK.position, aimTargetPosition, aimSharpness * Time.deltaTime);
     }
 }
