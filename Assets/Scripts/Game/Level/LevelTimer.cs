4d117e2960407b5653e5d2906a8d336734847315
CleryJa <daxxas158@gmail.com>
2023-02-08 01:14:20 +0100

    started level scripts

---
+++ Assets/Scripts/Game/Level/LevelTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Level
+{
+    public class LevelTimer : MonoBehaviour
+    {
+        private float currentTime = 0f;
+        private bool timerStarted = false;
+        
+        public void StartTimer()
+        {
+            timerStarted = true;
+        }
+        
+        public void StopTimer()
+        {
+            timerStarted = false;
+        }
+        
+        public void ResetTimer()
+        {
+            currentTime = 0f;
+        }
+        
+        private void Update()
+        {
+            if (timerStarted)
+            {
+                currentTime += Time.deltaTime;
+            }
+        }
+    }
+}
